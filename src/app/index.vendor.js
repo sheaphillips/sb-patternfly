62cbba11fce8705864a568193a77a3f649cc9732
Shea Phillips <shea.phillips@gmail.com>
2017-04-15 21:23:29 -0700

    - initial commit.

---
+++ src/app/index.vendor.js
@@ -0,0 +1,30 @@
+'use strict';
+
+// node_modules
+import "angular";
+
+import "angular-ui-router";
+
+
+import "angular-resource";
+
+import "angular-animate";
+
+import "angular-cookies";
+
+import "angular-touch";
+
+import "angular-sanitize";
+
+import "angular-messages";
+
+import "angular-aria";
+
+import "jquery";
+
+import "angular-resource";
+
+
+
+// local scripts
+//import "../assets/js/...";
